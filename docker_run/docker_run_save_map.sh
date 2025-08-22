5dab5199fcfbb88da22f37ecd6397b1299a5b99f
Ana Laura Soethe Chagas <ana.soethe.chagas@wzl-iqs.rwth-aachen.de>
2025-08-22 09:35:49 +0200

    Map can be saved

---
+++ docker_run/docker_run_save_map.sh
@@ -0,0 +1,1 @@
+docker exec -it cartographer bash -c "bash /save_map.sh; exec bash"
---
+++ entrypoint_scripts/save_map.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /opt/ros/humble/setup.bash
+source /colcon_ws/install/setup.bash
+
+# Set environment variables
+export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+export ROS_DOMAIN_ID=22
+
+ros2 run nav2_map_server map_saver_cli -t /map -f /map/my_map
