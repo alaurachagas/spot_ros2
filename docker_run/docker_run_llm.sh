df79d38e2795f4e5e84ad37f97f60e67bc396d09
Ana Laura Soethe Chagas <ana.soethe.chagas@wzl-iqs.rwth-aachen.de>
2025-11-22 16:47:59 +0100

    Added GPUs flag to LLM docker

--- docker_run/docker_run_llm.sh
+++ docker_run/docker_run_llm.sh
@@ -3,14 +3,19 @@
 # Enable GUI display forwarding
 xhost +local:docker
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_ROOT="$(dirname "$SCRIPT_DIR")"
+
 # Run the Docker Container with GUI, AI, and ROS 2 support
 docker run \
     -it \
     --rm \
+    --gpus all \
     --net=host \
     --privileged \
     --env="DISPLAY" \
     -v "$HOME/.Xauthority:/root/.Xauthority:rw" \
+    -v "$REPO_ROOT/colcon_ws/src/ai_agent_spot/saved_data:/overlay_ws/install/spot_agent/share/spot_agent/saved_data" \
     --name agent \
     ai-agent_spot_test:main \
     bash -c "bash /llm_entry.sh; exec bash"
