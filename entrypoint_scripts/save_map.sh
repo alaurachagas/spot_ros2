05fd869496ce407bb9702d3e392a6d1672ac231a
Ana Laura Soethe Chagas <ana.soethe.chagas@wzl-iqs.rwth-aachen.de>
2025-11-28 17:55:49 +0100

    validation configs

--- entrypoint_scripts/save_map.sh
+++ entrypoint_scripts/save_map.sh
@@ -7,4 +7,5 @@ source /colcon_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 export ROS_DOMAIN_ID=22
 
-ros2 run nav2_map_server map_saver_cli -t /map -f /map/validation_manual_11
+ros2 service call /write_state cartographer_ros_msgs/srv/WriteState "{filename: '/map/validation_map.pbstream', include_unfinished_submaps: true}"
+# ros2 run nav2_map_server map_saver_cli -t /map -f /map/validation_manual_11
